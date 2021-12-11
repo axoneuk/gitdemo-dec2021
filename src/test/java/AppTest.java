0294c4b33a6dc02cfe6a6838313c1ac421bd806b
ramduvvuri <ram.duvvuri@gmail.com>
2021-12-11 08:18:04 +0000

    Jira 2345 - Adding the project to SCM Repo

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("running test 3");
+    }
+
+}
